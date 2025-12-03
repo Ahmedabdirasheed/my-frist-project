30f571339ba68e2a4b9cbf56944b0cfafcc37b30
Ahmedkodah <axmedrashiid86@gmail.com>
2025-12-03 13:27:43 +0300

    Add files via upload

---
+++ lesson6.php
@@ -0,0 +1,14 @@
+<?php 
+echo (PI());
+echo "<br>";
+echo (max(2, 3, 5, -1, 100));
+echo "<br>";
+echo (min(-1, -100, 1));
+echo "<br>";
+echo (sqrt(49));
+echo "<br>";
+echo (abs(-100));
+echo "<br>";
+echo (round(9.123144));
+echo "<br>";
+?>
